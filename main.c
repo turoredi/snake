432ba8d9b8b8a507fd06e472034581c14ae65dc7
turoredi <adorjanpe123@gmail.com>
2024-01-30 13:45:47 +0100

    snake length now increases

--- main.c
+++ main.c
@@ -30,12 +30,8 @@ void init_field(char matrix[HEIGHT][WIDTH],int a){
 void init_snake(int matrix[199][2]){
     matrix[0][0] = 1;
     matrix[0][1] = 0;
-    for (int i = 1;i<9;i++){
-         matrix[i][0] = i+1;
-         matrix[i][1] = 0;
-    }
 }
-void print_game(char matrix[HEIGHT][WIDTH],int snake[snake_row][snake_col]) {
+void print_game(char matrix[HEIGHT][WIDTH],int snake[snake_row][snake_col],int snake_hossz) {
     char munka_matrix[HEIGHT][WIDTH];
     for (int i=0;i<HEIGHT;i++) {
         for (int j = 0;j<WIDTH;j++) {
@@ -45,7 +41,7 @@ void print_game(char matrix[HEIGHT][WIDTH],int snake[snake_row][snake_col]) {
         
     }
     munka_matrix[snake[0][0]][snake[0][1]] = '8';
-    for (int i = 1;i<9;i++){
+    for (int i = 1;i<snake_hossz;i++){
         munka_matrix[snake[i][0]][snake[i][1]] = '0';
     }
     for (int i = 0;i<WIDTH+2;i++){
@@ -81,7 +77,7 @@ int update_snake(char matrix[HEIGHT][WIDTH],int snake[snake_row][snake_col],char
         //önmaga
         bool hit = false;
         
-        for (int i = 1;i<9;i++){
+        for (int i = 1;i<=snake_hossz;i++){
             if (new_head[0][0] == snake[i][0] && new_head[0][1] == snake[i][1]){
                 hit = true;
             }
@@ -98,7 +94,7 @@ int update_snake(char matrix[HEIGHT][WIDTH],int snake[snake_row][snake_col],char
             return -1;}
         hit = false;
         
-        for (int i = 1;i<9;i++){
+        for (int i = 1;i<=snake_hossz;i++){
             
             if (new_head[0][0] == snake[i][0] && new_head[0][1] == snake[i][1]){
                 hit = true;
@@ -116,7 +112,7 @@ int update_snake(char matrix[HEIGHT][WIDTH],int snake[snake_row][snake_col],char
         if (new_head[0][0]==HEIGHT){
            
             return -1;}
-        for (int i = 1;i<9;i++){
+        for (int i = 1;i<=snake_hossz;i++){
             
             if (new_head[0][0] == snake[i][0] && new_head[0][1] == snake[i][1]){
                 hit = true;
@@ -135,7 +131,7 @@ int update_snake(char matrix[HEIGHT][WIDTH],int snake[snake_row][snake_col],char
         if (new_head[0][1]==WIDTH){
            
             return -1;}
-        for (int i = 1;i<9;i++){
+        for (int i = 1;i<=snake_hossz;i++){
             
             if (new_head[0][0] == snake[i][0] && new_head[0][1] == snake[i][1]){
                 hit = true;
@@ -146,17 +142,27 @@ int update_snake(char matrix[HEIGHT][WIDTH],int snake[snake_row][snake_col],char
             return -2;}
         break;
     }
-    snake_hossz--;
-    for (int i=snake_hossz;i>0;i--){
+
+
+    if (matrix[new_head[0][0]][new_head[0][1]] == 'a') {
+        matrix[new_head[0][0]][new_head[0][1]] = ' ';
+        snake_hossz++;
+        for (int i=snake_hossz-1;i>0;i--){
+            snake[i][0]=snake[i-1][0];
+            snake[i][1]=snake[i-1][1];
+        }
+        //snake[snake_hossz-1][0] = snake[snake_hossz-2][0];
+        //snake[snake_hossz-1][1] = snake[snake_hossz-2][1];
+        snake[0][0]=new_head[0][0];
+        snake[0][1]=new_head[0][1];
+        return 1;
+    }
+    for (int i=snake_hossz-1;i>0;i--){
         snake[i][0]=snake[i-1][0];
         snake[i][1]=snake[i-1][1];
     }
-    snake[0][0]=new_head[0][0];
-    snake[0][1]=new_head[0][1];
-    if (matrix[new_head[0][0]][new_head[0][1]] == 'a') {
-        matrix[new_head[0][0]][new_head[0][1]] = ' ';
-        return 1;
-    }
+        snake[0][0]=new_head[0][0];
+        snake[0][1]=new_head[0][1];
 }
 int main() {
     int rows = 199;
@@ -182,25 +188,26 @@ int main() {
     bool hiba = false;
     printf("Ez egy snake jatek.\nirányitas: a s w d\n");
     char direction;
-    print_game(tabla,snake_tabla);
+    print_game(tabla,snake_tabla,snake_hossz);
     while (!hiba && alma_count >0 && direction != EOF)
     {   
         scanf("%c",&direction);
-        print_game(tabla,snake_tabla);
+        print_game(tabla,snake_tabla,snake_hossz);
         if (direction == 'a' || direction == 's' || direction == 'd' || direction == 'w') {
             int update = update_snake(tabla,snake_tabla,direction,snake_hossz);
             if ( update == -1 || update == -2) {hiba = true;}
             else if (update == 1)
         {
                 alma_count--;
-                snake_hossz++;
+            snake_hossz++;
+
         }
         }   
         
     }
     
     if (alma_count==0){
-        print_game(tabla,snake_tabla);
+        print_game(tabla,snake_tabla,snake_hossz);
         printf("gratulalok");}
     else {printf( "hiba/leállitás történt.");}
     for(int i = 0; i < rows; i++) {
